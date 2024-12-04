0f9fd0b90d83243c9e83a23e0896d0dda6de4d76
eunsu317 <blue00920@gmail.com>
2024-12-04 22:29:03 +0900

    api 추가

---
+++ src/views/Home.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+      <h1>영화 목록</h1>
+      <div v-if="loading">로딩 중...</div>
+      <div v-else>
+        <div v-for="movie in movies" :key="movie.id">
+          <router-link :to="'/movie/' + movie.id">{{ movie.title }}</router-link>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  import axios from 'axios';
+  
+  export default {
+    data() {
+      return {
+        movies: [],
+        loading: true,
+      };
+    },
+    mounted() {
+      this.fetchMovies();
+    },
+    methods: {
+      fetchMovies() {
+        axios.get('a0e054e47b1ca4d58abcfc16ccf746bd') // 여기에 API URL 입력
+          .then(response => {
+            this.movies = response.data.results; // API 응답에 따라 조정
+            this.loading = false;
+          })
+          .catch(error => {
+            console.error(error);
+            this.loading = false; // 오류 발생 시 로딩 상태 종료
+          });
+      },
+    },
+  };
+  </script>
+  
+  <style>
+  /* 스타일 추가 가능 */
+  </style>
+  
